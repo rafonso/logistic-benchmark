45a41c94be4915015e79f1e488957515ec6eef60
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2022-02-13 15:24:42 +0000

    main function defined. commands now is languages

--- beanchmark.py
+++ beanchmark.py
@@ -6,14 +6,15 @@ import sys
 col_size = 10
 time_re = 'TOTAL_TIME (\d+)'
 
-def run_for_interations(commands,  x0, r, num_interations, repetitions):
+
+def run_for_interations(languages,  x0, r, num_interations, repetitions):
     time_interations = {}
     print(60 * "=")
     print("[{0}] {1} {2}".format(
         datetime.datetime.now().time(), "inter".rjust(col_size), str(num_interations).rjust(col_size)))
-    for description in commands.keys():
+    for language in languages.keys():
         time_interations.update(run_command(
-            description, commands.get(description), x0, r, num_interations, repetitions))
+            language, languages.get(language), x0, r, num_interations, repetitions))
     return {num_interations: time_interations}
 
 
@@ -29,43 +30,44 @@ def run_command(description, command_pattern, x0, r, num_interations, repetition
     return {description: deltaT}
 
 
-def print_results(results):
+def print_results(results, languages):
+    print(60 * "=")
     print("[{0}] RESULTS".format(datetime.datetime.now().time()))
     header = "inter".rjust(col_size)
-    for command in commands.keys():
-        header = header + command.rjust(col_size)
+    for language in languages.keys():
+        header = header + language.rjust(col_size)
     print(header)
     for interation in results.keys():
         line = str(interation).rjust(col_size)
         times = results.get(interation)
-        for command in commands.keys():
-            line = line + str(times.get(command)).rjust(col_size)
+        for language in languages.keys():
+            line = line + str(times.get(language)).rjust(col_size)
         print(line)
 
 
-########################################
-# 	  MAIN
-########################################
+def main():
+    x0 = float(sys.argv[1])
+    r = float(sys.argv[2])
+    repetitions = int(sys.argv[3])
 
-x0 = float(sys.argv[1])
-r = float(sys.argv[2])
-repetitions = int(sys.argv[3])
+    languages = {
+        "c": ".\\c-logistic-benchmark\\x64\\Debug\\c-logistic-benchmark.exe r {} {} {} {}",
+        "c#": ".\\cs-logistic-beanchmark\\bin\\Debug\\net6.0\\cs-logistic-beanchmark.exe r {} {} {} {}",
+        "java": "java -jar .\\java-logistic-benchmark\\logistic-benchmark\\target\\java-logistic-benchmark-jar-with-dependencies.jar r {} {} {} {}",
+        "node": "npm start --prefix typescript-logistic-benchmark -- r {} {} {} {}",
+        "python": "python .\\python-logistic-benchmark\\main.py r {} {} {} {}"
+    }
 
-commands = {
-    "c": ".\\c-logistic-benchmark\\x64\\Debug\\c-logistic-benchmark.exe r {} {} {} {}",
-    "c#": ".\\cs-logistic-beanchmark\\bin\\Debug\\net6.0\\cs-logistic-beanchmark.exe r {} {} {} {}",
-    "java": "java -jar .\\java-logistic-benchmark\\logistic-benchmark\\target\\java-logistic-benchmark-jar-with-dependencies.jar r {} {} {} {}",
-    "node": "npm start --prefix typescript-logistic-benchmark -- r {} {} {} {}",
-    "python": "python .\\python-logistic-benchmark\\main.py r {} {} {} {}"
-}
+    interations = [100, 1_000, 10_000, 100_000, 1_000_000, 5_000_000]
 
-interations = [100, 1_000, 10_000, 100_000, 1_000_000, 5_000_000]
+    results = {}
 
-results = {}
+    for num_interations in interations:
+        results.update(run_for_interations(
+            languages, x0, r, num_interations, repetitions))
 
-for num_interations in interations:
-    results.update(run_for_interations(
-        commands, x0, r, num_interations, repetitions))
-print(60 * "=")
+    print_results(results, languages)
 
-print_results(results)
+
+if __name__ == '__main__':
+    main()
