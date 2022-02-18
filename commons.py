6053c633771709bbd3fd6b30c0666129f409ac89
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2022-02-18 20:37:39 +0000

    Adjusts on Java project

--- commons.py
+++ commons.py
@@ -15,7 +15,7 @@ languages = {
     "c":        LangParams(".\\c-logistic-benchmark\\x64\\Debug\\c-logistic-benchmark.exe"),
     "c#":       LangParams(".\\cs-logistic-beanchmark\\bin\\Debug\\net6.0\\cs-logistic-beanchmark.exe"),
     "go":       LangParams(".\\go-logistic-benchmark\\go-logistic-benchmark.exe"),
-    "java":     LangParams("java -jar .\\java-logistic-benchmark\\logistic-benchmark\\target\\java-logistic-benchmark-jar-with-dependencies.jar"),
+    "java":     LangParams("java -jar .\\java-logistic-benchmark\\target\\java-logistic-benchmark-jar-with-dependencies.jar"),
     "kotlin":   LangParams("java -jar .\\kotlin-logistic-benchmark\\target\\kotlin-logistic-benchmark-jar-with-dependencies.jar"),
     "lua":      LangParams("lua .\\lua-logistic-benchmark\\main.lua", [5000000, 10000000]),
     "node":     LangParams("npm start --prefix typescript-logistic-benchmark --"),
