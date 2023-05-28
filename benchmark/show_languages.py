2b7fd50218f60a04931a1bbed1e843c2fe82987a
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2023-05-28 09:55:57 +0100

    show_languages refactored
    removed max interactions for C

--- benchmark/show_languages.py
+++ benchmark/show_languages.py
@@ -9,6 +9,7 @@ from commons import LangParams, change_work_dir, get_json_config_files
 @dataclass
 class ShowParams:
     only_codes: bool
+    commands: bool
     show_all: bool
 
 
@@ -18,48 +19,54 @@ def parse_args() -> ShowParams:
     )
     parser.add_argument("-c", "--only-codes", help="Show just codes",
                         action=argparse.BooleanOptionalAction)
+    parser.add_argument("-co", "--commands", help="Show codes and their commands",
+                        action=argparse.BooleanOptionalAction)
     parser.add_argument("-a", "--show-all", help="Show all details (e.g. color, max interactions, command)",
                         action=argparse.BooleanOptionalAction)
 
     args = parser.parse_args()
 
-    return ShowParams(args.only_codes, args.show_all)
+    return ShowParams(args.only_codes, args.commands, args.show_all)
 
 
-def print_codes(params):
-    print()
-    for param in params:
-        print(param.code)
-    print()
+def print_codes(params: list[LangParams]):
+    header = ["CODE"]
+    table = list(map(lambda param: [param.code], params))
+    print(tabulate(table, header))
 
 
-def get_header(show_params: ShowParams) -> list[str]:
+def print_commands(params: list[LangParams]):
+    header = ["CODE", "COMMAND"]
+    table = list(map(lambda param: [param.code, param.command], params))
+    print(tabulate(table, header))
+
+
+def print_all(params: list[LangParams]):
+    header = ["CODE", "NAME", "DESCRIPTION",
+              "MAX ITER", "COLOR", "LINE STYLE", "COMMAND"]
+    table = list(map(lambda param: [param.code, param.name, param.description,
+                 param.max_iter, param.color, param.linestyle, param.command], params))
+    print(tabulate(table, header, tablefmt="psql"))
+
+
+def print_name_description(params: list[LangParams]):
     header = ["CODE", "NAME", "DESCRIPTION"]
-    if(show_params.show_all):
-        header.extend(("MAX ITER", "COLOR", "LINE STYLE", "COMMAND"))
-
-    return header
-
-
-def get_table(params: list[LangParams], show_params: ShowParams) -> list[list[str]]:
-    if show_params.show_all:
-        return list(map(lambda param: [param.code, param.name, param.description, param.max_iter, param.color, param.linestyle, param.command], params))
-    else:
-        return list(map(lambda param: [param.code, param.name, param.description], params))
+    table = list(
+        map(lambda param: [param.code, param.name, param.description], params))
+    print(tabulate(table, header, tablefmt="psql"))
 
 
 def main():
     change_work_dir()
 
     show_params = parse_args()
-    params = get_json_config_files()
+    shower = print_codes if show_params.only_codes  \
+        else print_commands if show_params.commands \
+        else print_all if show_params.show_all      \
+        else print_name_description
 
-    if show_params.only_codes:
-        print_codes(params)
-    else:
-        header = get_header(show_params)
-        table = get_table(params, show_params)
-        print(tabulate(table, header, tablefmt="psql"))
+    params: list[LangParams] = get_json_config_files()
+    shower(params)
 
 
 if __name__ == '__main__':
