c5619e7f5fa165d977aa65973d98f97ff34d454a
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2023-05-28 08:33:13 +0100

    Added feature to show only the codes

--- benchmark/show_languages.py
+++ benchmark/show_languages.py
@@ -8,6 +8,7 @@ from commons import LangParams, change_work_dir, get_json_config_files
 
 @dataclass
 class ShowParams:
+    only_codes: bool
     show_all: bool
 
 
@@ -15,12 +16,21 @@ def parse_args() -> ShowParams:
     parser = argparse.ArgumentParser(
         description="Show Languages parameters"
     )
+    parser.add_argument("-c", "--only-codes", help="Show just codes",
+                        action=argparse.BooleanOptionalAction)
     parser.add_argument("-a", "--show-all", help="Show all details (e.g. color, max interactions, command)",
                         action=argparse.BooleanOptionalAction)
 
     args = parser.parse_args()
 
-    return ShowParams(args.show_all)
+    return ShowParams(args.only_codes, args.show_all)
+
+
+def print_codes(params):
+    print()
+    for param in params:
+        print(param.code)
+    print()
 
 
 def get_header(show_params: ShowParams) -> list[str]:
@@ -42,11 +52,14 @@ def main():
     change_work_dir()
 
     show_params = parse_args()
-    header = get_header(show_params)
     params = get_json_config_files()
-    table = get_table(params, show_params)
 
-    print(tabulate(table, header, tablefmt="psql"))
+    if show_params.only_codes:
+        print_codes(params)
+    else:
+        header = get_header(show_params)
+        table = get_table(params, show_params)
+        print(tabulate(table, header, tablefmt="psql"))
 
 
 if __name__ == '__main__':
